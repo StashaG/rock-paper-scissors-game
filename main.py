299204525130876735ff2eb3a51c64413f9c65f6
Stacey Graham <stacey.graham1106@gmail.com>
2024-03-08 13:09:11 +0000

    added print statements to get players moves

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from getpass import getpass as input
 print("EPIC Rock, Paper, Scissors GAME 🎲 🎮!")
 print()
 print(("Select your move (R, P, or S)"))
+print()
+player_1_move = input("Player 1: ")
+print()
+player_2_move = input("Player 2: ")
+print()
